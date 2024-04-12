0cbb1a650f3ddfab441bde97e4e41e3ae1d72ba8
Jose Tomaz <rbj.ribeiro@gmail.com>
2024-04-12 11:41:22 -0300

    feat: create module login

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,6 @@
-import './App.css';
-
-import { Button } from 'antd';
-import { useState } from 'react';
-import styled from 'styled-components';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import LoginScreen from './modules/login';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <StyledLink isBlue={count > 4}>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>Vite + React</h1>
-      </StyledLink>
-
-      <div className="card">
-        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <LoginScreen />;
 }
-
-interface PStyledLink {
-  isBlue?: boolean;
-}
-
-const StyledLink = styled.div<PStyledLink>`
-  color: ${(props) => (props.isBlue ? 'blue' : 'green')};
-  font-weight: bold;
-`;
-
 export default App;
