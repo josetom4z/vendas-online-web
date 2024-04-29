9c0dfe7da8c3ebd9313d375aa098f63e449df64d
Jose Tomaz <rbj.ribeiro@gmail.com>
2024-04-29 15:49:32 -0300

    feat: change layout and create button component

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,4 +1,7 @@
+import { Typography } from 'antd';
 import styled from 'styled-components';
+
+const { Title } = Typography;
 
 export const ContainerLoginScreen = styled.div`
   width: 100%;
@@ -16,7 +19,9 @@ export const BackgroundImage = styled.img`
   z-index: -1;
 `;
 
-export const LogoImage = styled.img``;
+export const LogoImage = styled.img`
+  width: 202px;
+`;
 
 export const ContainerLogin = styled.div`
   display: flex;
@@ -29,10 +34,15 @@ export const ContainerLogin = styled.div`
   padding: 22px;
 `;
 
+export const TitleLogin = styled(Title)`
+  color: #006397;
+`;
+
 export const LimitedContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   max-width: 498px;
   flex-direction: column;
+  align-items: center;
 `;
